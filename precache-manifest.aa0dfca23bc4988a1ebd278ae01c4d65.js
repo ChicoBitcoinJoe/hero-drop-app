695662dcd895432026ad691c1b863d485e33c888
Joseph Reed <chicobitcoinjoe@gmail.com>
2020-09-24 00:29:02 -0700

    Updates

---
+++ precache-manifest.aa0dfca23bc4988a1ebd278ae01c4d65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4f1a7d8625993b45f7587966a2b497",
+    "url": "/hero-drop-app/index.html"
+  },
+  {
+    "revision": "7b25ab7399c8966e5676",
+    "url": "/hero-drop-app/static/css/2.c7d94637.chunk.css"
+  },
+  {
+    "revision": "d7a3d6d5f7aff54cecad",
+    "url": "/hero-drop-app/static/css/main.5f367855.chunk.css"
+  },
+  {
+    "revision": "7b25ab7399c8966e5676",
+    "url": "/hero-drop-app/static/js/2.524733ea.chunk.js"
+  },
+  {
+    "revision": "31e7b09a2d178987207f4fdc89c27e08",
+    "url": "/hero-drop-app/static/js/2.524733ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a3d6d5f7aff54cecad",
+    "url": "/hero-drop-app/static/js/main.c7847f5e.chunk.js"
+  },
+  {
+    "revision": "5766c1c45ca84e73e8ce",
+    "url": "/hero-drop-app/static/js/runtime-main.6ca2a97a.js"
+  },
+  {
+    "revision": "203b271b717a07c4194cc3d6822aa6e3",
+    "url": "/hero-drop-app/static/media/bg.203b271b.png"
+  }
+]);
